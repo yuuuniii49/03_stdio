99baaac63f13c0bae9e051cf665804102d245966
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-09-17 16:25:38 +0900

    Revert "02"
    This reverts commit 994d360b7db94696512adeddba9d47e74f86bad7.

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
 	char c;
-		
-	printf("enter a character: ");
+	int i;
+	
+	printf("input a number :");
 	scanf("%c",&c);
 	
-	printf("the next character of %c (%i) is %c (%i)", c, c, c+1, c+1);
+	i=c-('0'-0);
+	printf("The input number is %i\n",i);
 	
 	return 0;
 }
